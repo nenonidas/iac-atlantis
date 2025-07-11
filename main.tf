b7230dd52b9f2427826c8bb94a9bc53a0928241c
nenonidas <alerodriguesdesouza@gmail.com>
2025-07-11 00:04:41 -0300

    feat: alterando comando de exemplo

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ terraform {
 
 resource "null_resource" "teste" {
   provisioner "local-exec" {
-    command = "echo Teste de Atlantis testes"
+    command = "echo Teste de Atlantis teste testes"
   }
 }
